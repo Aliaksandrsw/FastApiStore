78652090b7fccea3563b3d1a239d8cc6c36a1bac
Aliaksandrsw <swordswird@proton.me>
2024-08-06 21:41:39 +0300

    подлючение Alembic

--- app/models/products.py
+++ app/models/products.py
@@ -3,6 +3,7 @@ from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Float
 from sqlalchemy.orm import relationship
 from app.models import *
 
+
 class Product(Base):
     __tablename__ = 'products'
 

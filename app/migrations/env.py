550f4725e75d3ad1ab258f274b1d66d0606a0079
Aliaksandrsw <swordswird@proton.me>
2024-08-09 22:45:00 +0300

    Реализация аутентификации используя HTTP Basic Auth

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 from app.backend.db import Base
-from app.models import category, products
+from app.models import category, products, user
 
 target_metadata = Base.metadata
 
